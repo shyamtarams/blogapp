9bc67f8a99523bdbf7d4ddd7863e9b00a07ef7ea
shyamtarams <shyamkrishnanpv535@gmail.com>
2021-06-12 20:16:37 +0530

    accounts changes

--- accounts/views.py
+++ accounts/views.py
@@ -4,17 +4,20 @@ from .models import *
 
 def signup(request):
     if request.method=='POST':
-        unm=request.POST['username']
-        phone=request.POST['phone']
-        email=request.POST['email']
-        password=request.POST['pass']
-        login=Login(username=unm,password=password)
-        login.save()
-        data=Signup(username=unm,phone=phone,email=email,password=password,login=login)
-        data.save();
-        # return render(request,'signup.html',{'success':'registered'})
-        # return HttpResponse("success")
-        return redirect('/login')
+        try:
+            unm=request.POST['username']
+            phone=request.POST['phone']
+            email=request.POST['email']
+            password=request.POST['pass']
+            login=Login(username=unm,password=password)
+            login.save()
+            data=Signup(username=unm,phone=phone,email=email,password=password,login=login)
+            data.save();
+            return render(request,'signup.html',{'success':'registered'})
+            # return HttpResponse("success")
+            # return redirect('/login')
+        except Exception as err: 
+            return HttpResponse(err)
     else:
         return render(request,'signup.html')
 
@@ -22,10 +25,8 @@ def login(request):
     if request.method=='POST':
         try:
             un=request.POST['username']
-            # print(un)
             pw=request.POST['pass']
             user=Login.objects.get(username=un,password=pw)
-            # print(user.id)
             request.session['log_id']=user.id
             # return render(request,'profile.html',{'user':user})
             # return HttpResponse("logi suceesss")
